2e06c3d067ffaee52d5b39a90bfbec13785665dd
sammy ocharo <obanyisammy22@gmail.com>
2024-02-11 22:20:11 +0300

    initialize user entity class

---
+++ src/main/java/com/user/user/Entity/User.java
@@ -0,0 +1,71 @@
+package com.user.user.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.List;
+
+@Entity(name = "USER")
+public class User  {
+
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+    private String firstName;
+    private String secondName;
+    private String email;
+    private String role;
+
+    public User() {
+    }
+
+    public User(Long id,
+                String firstName,
+                String secondName,
+                String email,
+                String role) {
+        Id = id;
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.email = email;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
