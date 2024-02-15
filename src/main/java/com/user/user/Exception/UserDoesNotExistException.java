b4c6c114186a753994b02c497b99775e037bd12d
sammy ocharo <obanyisammy22@gmail.com>
2024-02-15 23:11:45 +0300

    add login mvc

--- src/main/java/com/user/user/Exception/UserDoesNotExistException.java
+++ src/main/java/com/user/user/Exception/UserDoesNotExistException.java
@@ -1,6 +1,7 @@
 package com.user.user.Exception;
 
 public class UserDoesNotExistException extends RuntimeException{
+    //exceptions
     public UserDoesNotExistException(String message) {
         super(message);
     }
